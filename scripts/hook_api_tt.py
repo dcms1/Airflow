7726d29b1bd29318743e2c93e9492d5df9c8e0c5
dcms1 <daniel.milagre@hotmail.com>
2022-06-06 23:42:31 -0300

    add paginate

--- scripts/hook_api_tt.py
+++ scripts/hook_api_tt.py
@@ -2,25 +2,23 @@ import requests
 import json
 import dotenv
 import os
-import datetime
-from datetime import timedelta,datetime
+from datetime import timedelta, datetime
 
-# dotenv.load_dotenv(dotenv.find_dotenv())
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+bearer_token_tt = os.getenv("bearer_token_tt")
 
 # To set your enviornment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 delta = timedelta(days=1)
-start_time = datetime.now().date() - delta
-end_time = datetime.now().date()
+start_time = f"{datetime.now().date()- delta}T00:00:00.00Z"
+end_time = f"{datetime.now().date()}T00:00:00.00Z"
 
-def auth():
-    print(os.getenv("bearer_token_tt"))
-    return os.getenv("bearer_token_tt")
+def auth() -> str:
+    return bearer_token_tt
 
-
-
-def create_url():
-    query = "spider-man"
+def create_url(start_time,end_time):
+    query = "doutor estranho"
     # Tweet fields are adjustable.
     # Options include:
     # attachments, author_id, context_annotations,
@@ -28,8 +26,9 @@ def create_url():
     # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
     # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
     # source, text, and withheld
-    tweet_fields = "tweet.fields=author_id,possibly_sensitive,conversation_id,created_at,text"
-    filters = f"start_time ={start_time}&end_time ={end_time}"
+    tweet_fields = "tweet.fields=author_id,conversation_id,created_at,text,public_metrics"
+    filters = f"start_time={start_time}&end_time={end_time}"
+    print(f"O dia de inicio das req é {start_time} e o final é {end_time}")
     url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}&{}".format(
         query, tweet_fields,filters
     )
@@ -37,7 +36,7 @@ def create_url():
 
 
 def create_headers(bearer_token):
-    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    headers = {"Authorization": f"Bearer {bearer_token}"}
     return headers
 
 
@@ -48,13 +47,22 @@ def connect_to_endpoint(url, headers):
         raise Exception(response.status_code, response.text)
     return response.json()
 
+def paginate(url, headers, next_token=""):
+    if next_token:
+        full_url = f"{url}&next_token={next_token}"
+    else:
+        full_url = url
+    data = connect_to_endpoint(full_url, headers)
+    yield data
+    if "next_token" in data.get("meta", {}):
+        yield from paginate(url, headers, data["meta"]["next_token"])
 
 def main():
     bearer_token = auth()
-    url = create_url()
+    url = create_url(start_time, end_time)
     headers = create_headers(bearer_token)
-    json_response = connect_to_endpoint(url, headers)
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    for json_response in paginate(url, headers):
+        print(json.dumps(json_response, indent=10, sort_keys=True))
 
 
 if __name__ == "__main__":
